99c4f3bd7e70556749244cf7666803721b1628c5
Jake Stambaugh <jake.stambaugh@gmail.com>
2021-03-17 01:17:44 -0400

    Loads a single question because we are not passing the id from python to javascript

--- static/main.js
+++ static/main.js
@@ -1,35 +1,60 @@
-function renderQuestions(questionArray) {
-    let questionContainer = document.getElementById("question-container");
-    let questionTemplate = document.getElementById("question-template");
-    questionArray.forEach(q => {
-        const node = questionTemplate.content.cloneNode();
-        console.log(node.children());
+function renderQuestion(q) {
+    console.log(q);
+    let existing = document.getElementById(`question-${q["id"]}`);
+    if (existing === null) {
+        let questionTemplate = document.getElementById("question-template");
+        const node = questionTemplate.content.firstElementChild.cloneNode(true);
+        node.id = `question-${q["id"]}`;
+
         let p = node.querySelector(".question .question-text");
         p.innerText = q["text"];
-        questionContainer.appendChild(p);
-    });
+
+        let upvotes = node.querySelector(".question .upvotes");
+        upvotes.innerText = q["upvotes"];
+        document.getElementById("question-container").appendChild(node);
+    } else {
+        let upvotes = existing.querySelector(".question .upvotes");
+        upvotes.innerText = q["upvotes"];
+    }
+}
+
+function renderQuestions(questionArray) {
+    let questionContainer = document.getElementById("question-container");
+    for(x = 0; x < questionArray.length; x++) {
+        renderQuestion(questionArray[x]);
+    }
 }
 
 function loadQuestions() {
     fetch(`/question`)
         .then(response => response.json())
         .then(data => {
+            console.log(data);
             renderQuestions(data);
+            let ids = data.map(q => q["id"]);
         });
 }
 
 function submitQuestion(event) {
+    console.log("running");
     event.preventDefault();
     let form = document.getElementById("question-form");
-    form.action = "POST";
-    form.method = "/question";
-    form.submit();
+    let data = new URLSearchParams();
+    let questionText = document.querySelector("#question-form input[type=textarea]");
+    data.append("question", questionText.value);
+    fetch("/question", { method: 'POST', body: data})
+        .then(data => {
+            loadQuestions();
+            questionText.value = "";
+        });
+}
 
-    loadQuestions();
+function renderComments(commentId) {
+    
 }
 
 function loadComments(questionId) {
-    fetch(`/question/{questionId}/comment`)
+    fetch(`/question/${questionId}/comment`)
         .then(response => response.json())
         .then(data => {
             console.log(data)
@@ -37,8 +62,10 @@ function loadComments(questionId) {
 }
 
 window.onload = function() {
+    const form = document.getElementById("question-form");
+    form.addEventListener("submit", submitQuestion);
     loadQuestions();
 
     // Reload all questions every 30s
-    window.setInterval(loadQuestions, 30000);
+    // window.setInterval(loadQuestions, 30000);
 }
